2749132522b399697e928b292905be51b04e9a57
Bahman Movaqar <Bahman@BahmanM.com>
2025-07-29 07:48:45 -0700

    [#78] Clean up the Outcome*.cs classes

--- src/BahmanM.Flow/Outcome.cs
+++ src/BahmanM.Flow/Outcome.cs
@@ -5,3 +5,29 @@ public static class Outcome
     public static Outcome<T> Success<T>(T value) => new Success<T>(value);
     public static Outcome<T> Failure<T>(Exception exception) => new Failure<T>(exception);
 }
+
+public static class OutcomeExtensions
+{
+    public static bool IsSuccess<T>(this Outcome<T> outcome) => outcome is Success<T>;
+    public static bool IsFailure<T>(this Outcome<T> outcome) => outcome is Failure<T>;
+
+    public static T GetOrElse<T>(this Outcome<T> outcome, T fallbackValue) =>
+        outcome is Success<T> s ? s.Value : fallbackValue;
+
+    internal static async Task<T> Unwrap<T>(this Task<Outcome<T>> outcomeTask)
+    {
+        var outcome = await outcomeTask;
+        return outcome switch
+        {
+            Success<T> s => s.Value,
+            Failure<T> f => throw f.Exception,
+            _ => throw new NotSupportedException("Unsupported outcome type.")
+        };
+    }
+}
+
+public sealed record Failure<T>(Exception Exception) : Outcome<T>;
+
+public sealed record Success<T>(T Value) : Outcome<T>;
+
+public abstract record Outcome<T>;
--- src/BahmanM.Flow/OutcomeTypes.cs
+++
@@ -1,7 +0,0 @@
-namespace BahmanM.Flow;
-
-public abstract record Outcome<T>;
-
-public sealed record Success<T>(T Value) : Outcome<T>;
-
-public sealed record Failure<T>(Exception Exception) : Outcome<T>;
